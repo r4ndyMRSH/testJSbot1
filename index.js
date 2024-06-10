696e417f52996c0cc3858d3b5edd1e9e6b6d6e89
Pavel Nazaranka <pvlnzr@gmail.com>
2024-06-10 15:29:09 +0300

    Adding error handler

--- index.js
+++ index.js
@@ -5,20 +5,40 @@
 //TestJSBot
 //pvlTest1bot
 
-import dotenv from 'dotenv';
-import {Bot} from "grammy";
+import dotenv from "dotenv";
+import { Bot, GrammyError, HttpError } from "grammy";
 
 dotenv.config();
 
 const bot = new Bot(process.env.BOT_API_KEY);
 
-//adding event listener
-//ctx - context
-bot.command('start', async (ctx)=>{
-    await ctx.reply("Hello, i am your bot!");
+//adding event listener (ctx - context)
+
+//Обработка команды старт
+bot.command("start", async (ctx) => {
+  await ctx.reply("Hello, i am your bot!");
 });
-bot.on('message', async (ctx)=>{
-    await ctx.reply("Thinking...");
+
+//Обработчик ошибок (нужны GrammyError, HttpError)
+
+bot.catch((err) => {
+  const ctx = err.ctx;
+  console.error(`Error while handling updaate ${ctx.update.update_id}`);
+  const e = err.error;
+
+  //Проверяем к какому типу относится ошибка
+  if (e instanceof GrammyError) {
+    console.log("Error  in request:", e.description);
+  } else if (e instanceof HttpError) {
+    console.log("Coul dnot contact Telegram:", e);
+  } else {
+    console.log("Unknown error", e);
+  }
+});
+
+//Ответ на входящее сообщение
+bot.on("message", async (ctx) => {
+  await ctx.reply("Thinking...");
 });
 
 //starting bot
