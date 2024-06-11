3a6ec2705de8200168db9e9fa9ac89a63ec159f4
pvlnzr <pvlnzr@gmail.com>
2024-06-11 10:49:25 +0300

    Adding message filtration

--- index.js
+++ index.js
@@ -12,11 +12,37 @@ dotenv.config();
 
 const bot = new Bot(process.env.BOT_API_KEY);
 
-//adding event listener (ctx - context)
+//Меню команд, массив объектов содержащий команды и их описание
 
-//Обработка команды старт
+bot.api.setMyCommands([
+  {
+    command: "start",
+    description: "Запуск бота",
+  },
+  {
+    command: "hello",
+    description: "Получить приветсвие",
+  },
+]);
+
+//adding event listener (ctx - context)
+//Обработка команд
 bot.command("start", async (ctx) => {
   await ctx.reply("Hello, i am your bot!");
+});
+
+//массив команд работает как оператор или, обрабатываем сразу несколько команд
+bot.command(["hello", "hi"], async (ctx) => {
+  await ctx.reply(`Hello, ${ctx.from.first_name}:)`);
+});
+
+//Ответ на определённые сообщения
+bot.hears("ping", async (ctx) => {
+  await ctx.reply("pong");
+});
+//мы можем использовать регулярные выражения для ответа на сообщение  котором используются определённые слова
+bot.hears([/жопа/, /fuck/], async (ctx) => {
+  await ctx.reply("Ругаться очень не красиво!");
 });
 
 //Обработчик ошибок (нужны GrammyError, HttpError)
@@ -36,9 +62,30 @@ bot.catch((err) => {
   }
 });
 
-//Ответ на входящее сообщение
-bot.on("message", async (ctx) => {
+//Ответ на входящее сообщение (крайне важна последовательность фильтров)
+//Можно использовать фильтр, для ответа на разные типы сообщений
+//так же как и в случае с коммандами можно обработать сразу несколько типов сообщений отправив массив в качестве типа сообщения
+
+//в этом случае мы парсим сообщение и отвечаем только на те где есть ссылки
+//можно сократить до "::url"
+bot.on(["message:entities:url", "::email"], async (ctx) => {
+  await ctx.reply("Получил ссылку");
+});
+bot.on("message:text", async (ctx) => {
+  //можно сократить до (:text)
+  let a = ctx.from.first_name;
   await ctx.reply("Thinking...");
+});
+
+//комбинация фильтров
+bot.on(":photo").on("::hashtag", async (ctx) => {
+  await ctx.reply("Получил фото с хештегом");
+});
+bot.on("message:photo", async (ctx) => {
+  await ctx.reply("Cool picture, man!");
+});
+bot.on("message:voice", async (ctx) => {
+  await ctx.reply("Nice voice:)");
 });
 
 //starting bot
